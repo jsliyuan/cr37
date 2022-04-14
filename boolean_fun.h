646c5b4d47bc5e27b820fb3cda5831e412d3413d
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-04-14 18:58:24 +0800

    Add files via upload

---
+++ boolean_fun.h
@@ -0,0 +1,203 @@
+#ifndef BOOLEAN_FUN_H
+#define BOOLEAN_FUN_H
+
+#include "affine_trans.h"
+
+#include <cstdarg>
+#include <string>
+
+class BooleanFun {
+  public:
+    // Returns the number of variables.
+    int var_num() const;
+
+    // Constructor with parameter (number of variables)
+    // Create an all-zero Boolean function in n variables.
+    BooleanFun(int n);
+
+    // Reload < opeartor
+    bool operator < (const BooleanFun &g) const {
+      if (this->n < g.var_num()) {
+        return true;
+      }
+      return this->get_anf() < g.get_anf();
+    }
+
+    // Reload = opeartor
+    bool operator == (const BooleanFun &g) const {
+      if (this->n != g.var_num()) {
+        return false;
+      }
+      return this->get_anf() == g.get_anf();
+    }
+
+    // Constructor with parameters, where n is the number
+    // of variables, and anf is the algebraic normal form.
+    // If anf is invalid, we throw an exception.
+    // For example,
+    // "0": the all-0 Boolean function
+    // "1": the all-1 Boolean function
+    // "x1x2+1": x1*x2+1
+    // "x1x2+x4x5": x1*x2+x4*x5
+    // We do not support parenthesis "()" for now.
+    // Do not include extra spaces.
+    BooleanFun(int n, std::string anf_str);
+
+    // Resets the ANF.
+    // Both truth table and anf are modified.
+    // Returns false if anf_str is invalid.
+    bool set_anf(std::string anf_str);
+
+    // Sets the coefficient d(x) of the ANF to constant c,
+    // where d is in [0, 2^n-1], and c is 0 or 1.
+    // If d or c is out of range, returns false.
+    bool set_anf_coe(int d, int c);
+
+    // Call this function after setting all the coefficients
+    // in the ANF.
+    // The truth table and degree will be re-computed.
+    void set_anf_coe_done();
+
+    // Set the truth_table[x] to v, where
+    // x is in [0, 2^n-1], and v is 0 or 1.
+    // Returns false if x or v is out of range.
+    bool set_truth_table(int x, int v);
+
+    // After setting the truth table, call this function.
+    // The degree and ANF will be computed then.
+    void set_truth_table_done();
+
+    // Destructor
+    ~BooleanFun();
+
+    // Returns the subfunction in n-1 variables by setting
+    // x_n to constant c, where c is 0 or 1.
+    BooleanFun sub_function(int c) const;
+
+    // Evaluate boolean function at a given point by giving {0,1}^n.
+    // We must have num = n; otherwise returns -1.
+    int value(int num, ...) const;
+
+    // Evaluate boolean function at a given point, where d
+    // is the decimal representation of a vector x in {0,1}^n.
+    // d in [0, 2^n-1], otherwise returns -1.
+    int value_dec(int d) const;
+
+    // Returns the algebraic normal form as a string. E.g.,
+    //   "0"
+    //   "1"
+    //   "x2+x1+x1x2"
+    // The terms are ordered by the lexicographical order (d(x)).
+    std::string get_anf() const;
+
+    // Returns anf[d], where d is in [0, 2^n-1].
+    // Returns -1 if d is out of range.
+    int get_anf_coe(int d) const;
+
+    // Returns the algebraic degree.
+    int get_degree() const;
+
+    // Compares if this Boolean function is equal to f.
+    bool is_equal(const BooleanFun& f) const;
+
+    // Takes the negation of this Boolean function, i.e.,
+    // let this = f + 1 (over GF(2)).
+    // Both truth table and anf are modified.
+    void negate();
+
+    // Let this = this + f (over GF(2)).
+    // Returns false if #vars(f) != n.
+    bool add(const BooleanFun& f);
+
+    // Let this = this * f (over GF(2)).
+    // Returns false if #vars(f) != n.
+    bool mult(const BooleanFun& f);
+
+    // Apply affine transformation to this Boolean function, i.e.,
+    // f = f(T(x)) = f(Ax + b), where T(x) = Ax + b.
+    // Returns false if the dimension does not match.
+    // Both truth table and anf are updated.
+    bool apply_affine_trans(const AffineTrans& trans);
+
+    // Trim all monomials whose degree is less than deg_upper.
+    // Both truth table and anf are updated.
+    void trim_degree_below(int deg_upper);
+
+    // Returns the Hamming distance between two Boolean functions.
+    // If their #variables do not match, return -1.
+    int dist(const BooleanFun& f) const;
+
+    // Returns true if this Boolean function is homogeneous
+    // as a polynomial over GF(2).
+    bool is_homogenous() const;
+
+    // Compute the Walsh transform on w, where w is in [0, 2^n-1]
+    // (w1, w2, ..., wn) -> d(w) := w_1*2^{n-1}+w_2*2^{n-2}+...+w_n.
+    // W(w) = sum_{x} (-1)^(f(x) + w*x)
+    int walsh_transform(int w) const;
+
+    // Returns the first-order nonlinearity, which is
+    // 2^{n-1} - max_w |walsh_transform(w)| / 2.
+    int nonlinearity() const;
+
+    // Returns the rth-order nonlinearity, where r >= 1.
+    int nonlinearity(int r) const;
+
+    // BE CAREFUL!
+    // truth table of this Boolean function, of length 2^n
+    // value of f(x_1, x_2, ..., x_n) is truth_table[d(x)],
+    // where d(x) := x_1*2^{n-1}+x_2*2^{n-2}+...+x_n.
+    int* truth_table;
+
+    // BE CAREFUL!
+    // algebraic normal form
+    // ANF[d(e)] is the coefficient of x1^{e1}x2^{e2}...xn^{en}.
+    int* anf;
+
+  private:
+    // number of variables
+    int n;
+
+    // algebraic degree
+    int degree;
+
+    // Compute truth table from anf.
+    void anf_to_truth_table();
+
+    // Compute anf from truth table.
+    void truth_table_to_anf();
+
+    // Returns the decimal representation of the given term.
+    // Example terms: "x1x2x3", "x3x11".
+    // Each term is of the form x^a = x1^{a1}x2^{a2}...xn^{an},
+    // where a is in {0,1}^n
+    // Returns the decimal value of the binary number (a1, a2, ..., an).
+    int get_term(std::string term) const;
+
+    // The inverse of the above function.
+    // Given the decimal value of the binary number (a1, a2, ..., an),
+    // returns the term x^a = prod_{i: a_i=1} xi.
+    std::string compose_term(int dec) const;
+
+    // Computes the mobius inversion of source[2^n], and writes
+    // the result into dest[2^n].
+    // dest[x] = sum_{y <= x bitwise} source[y]
+    // The summation is over GF(2).
+    void mobius_inversion(int* dest, int* source);
+
+    // Returns the base-2 weight of an integer， i.e., returns the
+    // number of one's in its binary representation.
+    int weight(int x) const;
+
+    // Computes the algebraic degree, and sets this->degree.
+    void compute_degree();
+
+    // Compute the inner product of x and y, viewed as vector {0,1}^n
+    int inner_product(int x, int y) const;
+
+    // Returns the rth-order nonlinearity, where r >= 1.
+    // Cut down the search if the current value is > upper_bound.
+    int nonlinearity(int r, int upper_bound) const;
+};
+
+#endif
---
+++ example.cpp
@@ -0,0 +1,336 @@
+#include "boolean_fun.h"
+#include "affine_trans.h"
+
+#include <iostream>
+#include <set>
+#include<vector>
+#include <iostream>  // I/O 
+#include <fstream>   // file I/O
+#include <sstream>
+#include <iomanip>   // format manipulation
+#include <string>
+using namespace std;
+
+void read_boolean_fun( string line, BooleanFun *f)
+{
+    stringstream stringin(line);
+    for (int i = 0; i < (1<<6); i ++) {
+      int y = 0, x = i;
+    for (int j = 0; j < 6; j ++) {
+      y = y*2 + x % 2;
+      x = x / 2;
+    }
+
+    int v;
+    stringin >> v;
+    f->set_truth_table(y, v);
+    }
+  f->set_truth_table_done();
+}
+
+
+int count_all_mod_rm36(string fni,vector<AffineTrans>& all_trans) {
+  BooleanFun fn1(6);
+   AffineTrans t6_rand(6);
+  set<string> all_bfs;
+  int total = 0;
+
+  while (1) {
+     
+    t6_rand.set_random();
+    fn1.set_anf(fni);
+  	fn1.apply_affine_trans(t6_rand);
+  	fn1.trim_degree_below(4);
+
+  	string anf = fn1.get_anf();
+  	if (all_bfs.find(anf) == all_bfs.end()) {
+      //cout<<t6_rand.get_a_str()<<endl;
+     // printf("\n");
+      t6_rand.inverse();
+   // cout<<t6_rand.get_a_str()<<endl;
+    all_trans.push_back(t6_rand);
+      all_bfs.insert(anf);
+      total ++;
+     cout << total << endl;
+    }
+    if(total==13888)
+    break;
+  }
+
+  return total;
+}
+
+int main() {
+  /*
+ 
+  cout << count_all_mod_rm36("x1x2x4x5+x1x2x3x6") << endl;
+
+  cout << count_all_mod_rm36("x1x2x3x4x5") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5+x1x2x3x6") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5+x1x3x4x6+x1x2x5x6") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5x6") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5x6+x1x2x3x4") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5x6+x1x2x4x5+x1x2x3x6") << endl;
+  cout << count_all_mod_rm36("x1x2x3x4x5x6+x2x3x4x5+x1x3x4x6+x1x2x5x6") << endl;
+  */
+//  cout << count_all_mod_rm36("x1x2x4x5+x1x2x3x6") << endl;
+
+//cout << count_all_mod_rm36("x1x2x3x4") << endl;
+ifstream fin;
+ifstream ffin;
+ifstream fin1;
+string line;
+string line1;
+string line2;
+string line3;
+string line4;
+BooleanFun g(6);
+BooleanFun h(6);
+BooleanFun g3(6);
+set<string> fn3_14;
+set<string> H3_6_8;
+set<string> fn10_13_15;
+set<string> magic8;
+set<string> F3_14;
+set<string> bad_g;
+set<string>  fn6_6;
+vector<AffineTrans> all_trans;
+
+int num;
+fin.open( "fn3_14.txt", ios::in);  
+ if(fin.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin.eof())
+{   
+  string anf3_14;
+  BooleanFun *f1 = new BooleanFun(6);
+    getline(fin,line);
+      stringstream stringin(line);
+    read_boolean_fun(line,f1);
+    anf3_14=f1->get_anf();
+   fn3_14.insert(anf3_14);
+   F3_14.insert(f1->get_anf());
+}
+fin.close();
+
+/*for(set<string>::iterator it=F3_14.begin();it!=F3_14.end();it++)
+{
+    cout<<*it<<endl;
+    num=num+1;
+}
+printf("%d",num);*/
+
+ffin.open( "H3_6_8.txt", ios::in);  
+if(ffin.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!ffin.eof())
+{
+  string anf_h368;
+   BooleanFun *f1 = new BooleanFun(6);
+    getline(ffin,line1);
+      stringstream stringin(line1);
+    read_boolean_fun(line1,f1);
+    anf_h368=f1->get_anf();
+   H3_6_8.insert(anf_h368);
+}
+ ffin.close();
+
+/*for(set<string>::iterator it=H3_6_8.begin();it!=H3_6_8.end();it++)
+{
+    cout<<*it<<endl;
+    num=num+1;
+}
+printf("%d",num);*/
+
+ int num2=0;
+ int start_idx,end_idx;
+ cin>>start_idx;
+ cin>>end_idx;
+fin1.open( "fn10_13_15.txt", ios::in);  
+if(fin1.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin1.eof())
+{ 
+  getline(fin1,line2);
+  num2=num2+1;
+  if(num2>end_idx)
+    break;
+  if(num2>=start_idx&&num2<=end_idx)
+  {
+  string anf_f10;
+ BooleanFun *f1 = new BooleanFun(6);
+      stringstream stringin(line2);
+    read_boolean_fun(line2,f1);
+  f1->set_truth_table_done();
+  anf_f10=f1->get_anf();
+   fn10_13_15.insert(anf_f10);
+  } 
+}
+
+/*for(set<string>::iterator it=fn10_13_15.begin();it!=fn10_13_15.end();it++)
+{
+    cout<<*it<<endl;
+    num2=num2+1;
+}
+ printf("%d \n",num2);*/
+ fin1.close();
+
+  num2=0;
+fin1.open( "6_s8_1.txt", ios::in);  
+if(fin1.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin1.eof())
+{ 
+  string anf_magic8;
+  BooleanFun *f1 = new BooleanFun(6);
+    getline(fin1,line3);
+      stringstream stringin(line3);
+    read_boolean_fun(line3,f1);
+    anf_magic8=f1->get_anf();
+   magic8.insert(anf_magic8);
+}
+for(set<string>::iterator it=magic8.begin();it!=magic8.end();it++)
+{
+    cout<<*it<<endl;
+    num2=num2+1;
+}
+ printf("%d \n",num2);
+ fin1.close();
+
+num2=0;
+fin1.open( "f6_6.txt", ios::in);  
+if(fin1.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin1.eof())
+{ 
+  string anf_32;
+  BooleanFun *f1 = new BooleanFun(6);
+    getline(fin1,line4);
+      stringstream stringin(line4);
+    read_boolean_fun(line4,f1);
+    anf_32=f1->get_anf();
+   fn6_6.insert(anf_32);
+}
+for(set<string>::iterator it=fn6_6.begin();it!=fn6_6.end();it++)
+{
+    cout<<*it<<endl;
+    num2=num2+1;
+}
+ printf("%d \n",num2);
+ fin1.close();
+
+num2=0;
+count_all_mod_rm36("x2x3x4x5+x1x3x4x6+x1x2x5x6", all_trans);
+for(vector<AffineTrans>::iterator it=all_trans.begin();it!=all_trans.end();it++)
+{
+    cout<<it->get_a_str()<<endl;
+    printf("\n");
+    num2=num2+1;
+}
+cout<<num2<<endl;
+
+int flag = 0;
+int flag1 = start_idx;
+
+for(set<string>::iterator g=fn10_13_15.begin();g!=fn10_13_15.end();g++)
+{
+  set<string> inter; 
+  set<string> add_f_g; // 存放fn3_14+g
+  string anf_g;
+  anf_g=*g; 
+  cout<<anf_g<<endl;
+  BooleanFun g3(6,anf_g); //重构booleanfun g3
+
+//g+h\in f3_14 
+   for(set<string>::iterator h=H3_6_8.begin();h!=H3_6_8.end();h++){
+    string anf_h;
+    anf_h=*h; 
+    BooleanFun hg(6,anf_h); //重构booleanfun h
+    hg.add(g3);// h+g=hg
+    string anf;
+    anf=hg.get_anf();
+
+      if (fn3_14.find(anf) != fn3_14.end()) {
+       // O(1)
+      inter.insert(anf_h);  // hg + g = h
+    }
+  }
+  //Enumerate A
+    for (vector<AffineTrans>::iterator trans=all_trans.begin();trans!=all_trans.end();trans++ ) {
+    bool all_in = true;
+    bool all_in_32 = true;
+      for (set<string>::iterator magic=magic8.begin();magic!=magic8.end();magic++) {
+        string anf2;
+        anf2= *magic;
+        BooleanFun fm(6,anf2) ;
+        fm.apply_affine_trans(*trans);
+        fm.trim_degree_below(3);
+        string anf=fm.get_anf();
+        //cout<<anf<<endl;
+        if (inter.find(anf) == inter.end())
+        {
+          all_in = false;
+          break;
+        }
+      }
+    if (all_in) {
+
+      for (set<string>::iterator magic32=fn6_6.begin();magic32!=fn6_6.end();magic32++) {
+          string anf_magic32;
+          anf_magic32=*magic32;
+          BooleanFun fmm(6,anf_magic32) ;//重构fn6_6
+        fmm.apply_affine_trans(*trans);
+        fmm.trim_degree_below(3);
+        fmm.add(g3);
+        string anf7=fmm.get_anf();
+      
+          if(fn3_14.find(anf7)==fn3_14.end())
+          {
+         //   cout<<anf7<<endl;
+            all_in_32 = false;
+            break;
+          }
+      }
+      
+      if(all_in_32)
+      {
+        bad_g.insert(*g);
+      cout << "\n Bad news! Need more work!" << endl;
+      cout << trans->get_a_str() << endl;
+      cout << *g << endl;
+      flag=flag+1;
+      } 
+      }
+  }
+  cout<< flag1++<<" ";
+}
+cout<<"find "<<flag<<endl;
+
+//输出不希望的g集合
+for (set<string>::iterator n=bad_g.begin();n!=bad_g.end();n++) {
+      string anf_g1;
+      anf_g1= *n;
+      cout<<anf_g1<<endl;  
+    }
+  return 0;
+}
