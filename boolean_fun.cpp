94a11b0640bea451b6d40030d8b7f972f74b8008
jjgaoRita <97389626+jjgaoRita@users.noreply.github.com>
2022-04-19 19:08:21 +0800

    Add files via upload

---
+++ boolean_fun.cpp
@@ -0,0 +1,641 @@
+#include "boolean_fun.h"
+#include <memory.h>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+// BooleanFun constructor with parameter (number of variables)
+BooleanFun::BooleanFun(int n) {
+  this->n = n;
+  truth_table = new int[1<<n];
+  anf = new int[1<<n];
+  for (int i = 0; i < (1<<n); i ++) {
+    truth_table[i] = 0;
+    anf[i] = 0;
+  }
+  degree = 0;
+}
+
+// Constructor with parameters, where n is the number
+// of variables, and anf_str is the algebraic normal form.
+// For example,
+//   anf = "x1x2+x3+1"
+BooleanFun::BooleanFun(int n, string anf_str) {
+  this->n = n;
+  truth_table = new int[1<<n];
+  anf = new int[1<<n];
+  for (int i = 0; i < (1<<n); i ++) {
+    truth_table[i] = 0;
+    anf[i] = 0;
+  }
+
+  this->set_anf(anf_str);
+
+  // Compute truth_table using anf
+  anf_to_truth_table();
+  compute_degree();
+}
+
+// Allocate memory and copy all data from g to this.
+void BooleanFun::copy_data(const BooleanFun& g) {
+  this->n = g.var_num();
+  this->degree = g.get_degree();
+  anf = new int[1<<n];
+  truth_table = new int[1<<n];
+
+  memcpy(this->anf, g.anf, (1<<n)*sizeof(int));
+  memcpy(this->truth_table, g.truth_table, (1<<n)*sizeof(int));
+}
+
+// Copy constructor.
+BooleanFun::BooleanFun(const BooleanFun& g) {
+  copy_data(g);
+}
+
+// Assignment operator
+BooleanFun& BooleanFun::operator=(const BooleanFun& g) {
+  if (anf) {
+    delete anf;
+  }
+  if (truth_table) {
+    delete truth_table;
+  }
+
+  copy_data(g);
+  return *this;
+}
+
+// Returns the algebraic normal form of the Boolean function.
+std::string BooleanFun::get_anf() const {
+  std::string result = "";
+  for (int i = 0; i < (1<<n); i ++) {
+    if (anf[i] == 1) {
+      if (result.size() > 0) {
+        result += "+";
+      }
+      result += compose_term(i);
+    }
+  }
+  if (result.size() == 0) {
+    result = "0";
+  }
+
+  return result;
+}
+
+// Returns anf[2^n] as a 01 string of length 2^n.
+// The order preserves.
+string BooleanFun::get_coe_list() const {
+  string result = "";
+  for (int i = 0; i < (1<<n); i ++) {
+    if (anf[i] == 0) {
+      result += "0";
+    }
+    if (anf[i] == 1) {
+      result += "1";
+    }
+    if (anf[i] != 0 && anf[i] != 1) {
+      cout << "ERROR: invalid anf[" << i << "] = " << anf[i] << endl;
+    }
+  }
+
+  return result;
+}
+
+// Returns anf[d], where d is in [0, 2^n-1].
+int BooleanFun::get_anf_coe(int d) const {
+  if (d < 0 || d >= (1<<n)) {
+    return -1;
+  }
+
+  return anf[d];
+}
+
+// Set the truth_table[x] to v, where
+// x is in [0, 2^n-1], and v is 0 or 1.
+// Returns false if x or v is out of range.
+bool BooleanFun::set_truth_table(int x, int v) {
+  if (x < 0 || x >= (1<<n)) {
+    return false;
+  }
+  if (v < 0 || v > 1) {
+    return false;
+  }
+
+  truth_table[x] = v;
+  return true;
+}
+
+// After setting the truth table, call this function.
+// The degree and ANF will be computed then.
+void BooleanFun::set_truth_table_done() {
+  this->truth_table_to_anf();
+  this->compute_degree();
+}
+
+// Resets the ANF.
+// Both truth table and anf are modified.
+// Returns false if anf_str is invalid.
+bool BooleanFun::set_anf(std::string anf_str) {
+  // Clears the orginal anf.
+  for (int i = 0; i < (1<<n); i ++) {
+    anf[i] = 0;
+  }
+
+  // Filter anf_str, igonring extrac spaces etc.
+  string str;
+  const string VALID_ANF_CHARS = "0123456789x+()";
+  for (int i = 0; i < anf_str.size(); i ++) {
+    if (VALID_ANF_CHARS.find(anf_str.at(i)) != std::string::npos) {
+      str = str + anf_str.at(i); 
+    }
+  }
+  anf_str = str;
+
+  string term;
+  // Split anf by '+'; for each term, set the coefficient to +1.
+  int i = 0;
+  while (i <= anf_str.size()) {
+    if (i == anf_str.size() || anf_str.at(i) == '+') {
+      // Constant term "0" or "1"
+      if (term == "1") {
+        anf[0] = (anf[0] + 1) % 2;
+      }
+      if (term.size() > 1) {
+        int d = get_term(term);
+        anf[d] = (anf[d] + 1) % 2;
+      }
+      term = "";
+    }  else {
+      term += anf_str.at(i);
+    }
+
+    i ++;
+  }
+
+  // Compute truth_table using anf
+  anf_to_truth_table();
+  compute_degree();
+
+  return true;
+}
+
+// Resets the ANF given the coefficient list.
+// Returns false if coe_list is invalid, where
+// coe_list should be a 01 string of length 2^n.
+bool BooleanFun::set_coe_list(std::string coe_list) {
+  int idx = 0;
+  for (int i = 0; i < (1<<n); i ++) {
+    while (idx < coe_list.length() &&
+           coe_list[idx] != '0' && coe_list[idx] != '1') {
+      idx ++;
+    }
+    if (idx >= coe_list.length()) {
+      cout << "ERROR: coe_list is too short!";
+      return false;
+    }
+    anf[i] = (coe_list[idx] == '1' ? 1 : 0);
+    idx ++;
+  }
+
+  // Compute truth_table using anf
+  anf_to_truth_table();
+  compute_degree();
+
+  return true;
+}
+
+// Sets the coefficient d of the ANF to constant c,
+// where d is in [0, 2^n-1], and c is 0 or 1.
+bool BooleanFun::set_anf_coe(int d, int c) {
+  if (d < 0 || d >= (1 << n)) {
+    return false;
+  }
+  if (c < 0 || c > 1) {
+    return false;
+  }
+  anf[d] = c;
+
+  return true;
+}
+
+void BooleanFun::set_anf_coe_done() {
+  // Compute truth_table using anf
+  anf_to_truth_table();
+  compute_degree();
+}
+
+// Computes the mobius inversion of source[2^n], and writes
+// the result into dest[2^n].
+// dest[x] = XOR_{y <= x bitwise} source[y]
+void BooleanFun::mobius_inversion(int* dest, int* source) {
+  // Initialize
+  for (int i = 0; i < (1<<n); i ++) {
+    dest[i] = 0;
+  }
+
+  for (int j = 0; j < (1<<n); j ++) {
+    if (source[j] == 0)
+      continue;
+
+    for (int i = 0; i < (1<<n); i ++) {
+      // if j <= i bitwise, i.e., j | i = i
+      if ((j | i) == i) {
+        dest[i] = (dest[i] + source[j]) % 2;
+      }
+    }
+  }
+}
+
+// Compute truth table from algebraic normal form.
+void BooleanFun::anf_to_truth_table()
+{
+  this->mobius_inversion(truth_table, anf);
+}
+
+// Compute algebraic normal form from truth table.
+void BooleanFun::truth_table_to_anf()
+{
+  this->mobius_inversion(anf, truth_table);
+}
+
+// Returns the decimal representation of the given term.
+int BooleanFun::get_term(std::string term) const {
+  int binary[n];
+  for (int i = 0; i < n; i ++)
+    binary[i] = 0;
+
+  int i = 0;
+  int v = 0;
+  term = term + "x";
+  while (i < term.size()) {
+    if (i == term.size() || term.at(i) == 'x') {
+      if (i > 0) {
+        binary[v-1] = 1;
+      }
+      v = 0;
+    } else {
+      // Skip chars other than '0' - '9'
+      v = v*10 + term.at(i) - '0';
+    }
+    i ++;
+  }
+
+  // Convert binary[] to a decimal number.
+  int result = 0;
+  for (i = 0; i < n; i ++) {
+    result = result*2 + binary[i];
+  }
+  return result;
+}
+
+// The inverse of get_term()
+std::string BooleanFun::compose_term(int dec) const {
+  if (dec == 0) {
+    return "1";
+  }
+
+  string term;
+  for (int i = n; i >= 1; i --) {
+    if (dec % 2 == 1) {
+      term = "x" + std::to_string(i) + term;
+    }
+    dec = dec / 2;
+  }
+  return term;
+}
+
+// Computes this->degree.
+void BooleanFun::compute_degree() {
+  int deg = 0;
+  for (int i = 0; i < (1<<n); i ++) {
+    if (anf[i] == 1 && weight(i) > deg) {
+      deg = weight(i);
+    }
+  }
+  this->degree = deg;
+}
+
+// Returns the algebraic degree.
+int BooleanFun::get_degree() const {
+  return this->degree;
+}
+
+// Returns the subfunction in n-1 variables by setting
+// x_n to constant c.
+BooleanFun BooleanFun::sub_function(int c) const {
+  BooleanFun sub(n-1);
+  for (int x = 0; x < (1<<n); x ++) {
+    int xn = x % 2;
+    if (xn == c) {
+      sub.set_truth_table(x / 2, truth_table[x]);
+    }
+  }
+  sub.set_truth_table_done();
+  return sub;
+}
+
+// BooleanFun destructor
+BooleanFun::~BooleanFun()
+{
+  delete truth_table;
+  delete anf;
+}
+
+// Returns the number of variables.
+int BooleanFun::var_num() const {
+  return n;
+}
+
+// Returns the base-2 weight of an integer， i.e., returns the
+// number of one's in its binary representation.
+int BooleanFun::weight(int x) const {
+  int sum = 0;
+  while (x > 0) {
+    sum += x % 2;
+    x = x/2;
+  }
+
+  return sum;
+}
+
+// Evaluate the Boolean function at a given point.
+int BooleanFun::value(int num, ...) const {
+  if (num != n) {
+    return -1;
+  }
+  va_list valist;
+
+  int dec = 0;
+  va_start(valist, num);
+  for (int i = 0;  i < num; i ++) {
+    dec = dec*2 + va_arg(valist, int);
+  }
+  va_end(valist);
+  return truth_table[dec];
+}
+
+// Evaluate the Boolean function at a given point,
+// given the decimal representation of a point.
+int BooleanFun::value_dec(int d) const {
+  if (d < 0 || d >= (1<<n)) {
+    return -1;
+  }
+
+  return truth_table[d];
+}
+
+// Checks if this Boolean function equals f.
+bool BooleanFun::is_equal(const BooleanFun& f) const {
+  if (f.var_num() != n) {
+    return false;
+  }
+
+  for (int i = 0; i < (1<<f.var_num()); i ++) {
+    if (f.value_dec(i) != truth_table[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Let this = 1 + this (over GF(2)). That is,
+// take the negation.
+void BooleanFun::negate() {
+  for (int i = 0; i < (1<<n); i ++) {
+    this->truth_table[i] = (this->truth_table[i] + 1) % 2;
+  }
+
+  this->anf[0] = (1 + this->anf[0]) % 2;
+}
+
+// Let this = this + f (over GF(2)).
+// Returns false if #vars(f) != n.
+bool BooleanFun::add(const BooleanFun& f) {
+  if (n != f.var_num()) {
+    return false;
+  }
+
+  for (int i = 0; i < (1<<n); i ++) {
+    this->truth_table[i] = (this->truth_table[i] + f.truth_table[i]) % 2;
+  }
+
+  this->truth_table_to_anf();
+  this->compute_degree();
+
+  return true;
+}
+
+// Let this = this * f (over GF(2)).
+// Returns false if #vars(f) != n.
+bool BooleanFun::mult(const BooleanFun& f) {
+  if (n != f.var_num()) {
+    return false;
+  }
+
+  for (int i = 0; i < (1<<n); i ++) {
+    this->truth_table[i] = this->truth_table[i] * f.truth_table[i];
+  }
+
+  this->truth_table_to_anf();
+  this->compute_degree();
+
+  return true;
+}
+
+// Trim all monomials whose degree is less than deg_upper.
+void BooleanFun::trim_degree_below(int deg_upper) {
+  bool modified = false;
+
+  for (int i = 0; i < (1<<n); i ++) {
+    if (this->weight(i) < deg_upper && anf[i] > 0) {
+      anf[i] = 0;
+      modified = true;
+    }
+  }
+  if (modified) {
+    this->anf_to_truth_table();
+    if (deg_upper > this->degree) {
+      this->degree = 0;
+    }
+  }
+}
+
+// Apply affine transformation to this Boolean function, i.e.,
+// f = f(T(x)) = f(Ax + b), where T(x) = Ax + b.
+// Returns false if the dimension does not match.
+// Both truth table and anf are updated.
+bool BooleanFun::apply_affine_trans(const AffineTrans& trans) {
+  if (this->n != trans.get_n()) {
+    return false;
+  }
+
+  int* new_tt;
+  new_tt = new int[1<<n];
+  for (int i = 0; i < (1<<n); i ++) {
+    int ti = trans.apply(i);
+    new_tt[i] = this->truth_table[ti];
+  }
+
+  // Copy new_tt to truth_table
+  for (int i = 0; i < (1<<n); i ++) {
+    this->truth_table[i] = new_tt[i];
+  }
+  delete new_tt;
+
+  // Updates ANF
+  this->truth_table_to_anf();
+
+  return true;
+}
+
+// Returns the Hamming distance between two Boolean functions.
+// If their #variables do not match, return -1.
+int BooleanFun::dist(const BooleanFun& f) const {
+  if (this->n != f.var_num()) {
+    return -1;
+  }
+
+  int total = 0;
+  for (int i = 0; i < (1<<n); i ++) {
+    if (f.value_dec(i) != truth_table[i]) {
+      total ++;
+    }
+  }
+  return total;
+}
+
+// Returns true if this Boolean function is homogeneous
+// as a polynomial over GF(2).
+bool BooleanFun::is_homogenous() const {
+  for (int i = 0; i < (1 << n); i ++) {
+    if (anf[i] == 1 && weight(i) != degree)
+      return false;
+  }
+  return true;
+}
+
+// Compute the Walsh transform on w, where w is in [0, 2^n-1]
+// (w1, w2, ..., wn) -> d(w) := w_1*2^{n-1}+w_2*2^{n-2}+...+w_n.
+// W(w) = sum_{x} (-1)^(f(x) + w*x)
+int BooleanFun::walsh_transform(int w) const {
+  int sum = 0;
+  for (int i = 0; i < (1 << n); i ++) {
+    int parity = truth_table[i] + inner_product(i, w);
+    if (parity % 2 == 0) {
+      sum += 1;
+    } else {
+      sum -= 1;
+    }
+  }
+
+  return sum;
+}
+
+// Compute the inner product of x and y, viewed as vector {0,1}^n
+int BooleanFun::inner_product(int x, int y) const {
+  int prod = x & y;
+  int sum = 0;
+  while (prod > 0) {
+    sum += prod % 2;
+    prod = prod / 2;
+  }
+
+  return sum;
+}
+
+// Returns the first-order nonlinearity, which is
+// 2^{n-1} - max_w |walsh_transform(w)| / 2.
+int BooleanFun::nonlinearity() const {
+  int max_w = 0;
+  int sum[(1<<n)];
+  for (int w = 0; w < (1 << n); w ++) {
+    sum[w] = 0;
+  }
+
+  int parity;
+  int val;
+  for (int i = 0; i < (1 << n); i ++) {
+    int val = truth_table[i];
+    for (int w = 0; w < (1 << n); w ++) {
+      parity = val + inner_product(i, w);
+      if (parity % 2 == 0) {
+        sum[w] += 1;
+      } else {
+        sum[w] -= 1;
+      }
+    }
+  }
+
+  for (int w = 0; w < (1 << n); w ++) {
+    if (sum[w] > max_w) {
+      max_w = sum[w];
+    }
+    if (-sum[w] > max_w) {
+      max_w = -sum[w];
+    }
+  }
+
+  return (1<<(n-1)) - max_w/2;
+}
+
+// Returns the rth-order nonlinearity.
+// Cut the search if existing value is > upper_bound
+int BooleanFun::nonlinearity(int r, int upper_bound) const {
+  if (r >= degree) {
+    return 0;
+  }
+  if (r == 1) {
+    return nonlinearity();
+  }
+
+  // Find all x in [0, 2^(n-1)-1] with weight r.
+  vector<int> deg_r_x;
+  for (int i = 0; i < (1<<(n-1)); i ++) {
+    if (weight(i) == r) {
+      deg_r_x.push_back(i);
+    }
+  }
+
+  // Enumerates all homogenous functions of degree r, including 0.
+  BooleanFun homr(n-1);
+  while (1) {
+    BooleanFun sub0 = this->sub_function(0);
+    sub0.add(homr);
+    int non0 = sub0.nonlinearity(r-1, upper_bound);
+    if (non0 < upper_bound) {
+      BooleanFun sub1 = this->sub_function(1);
+      sub1.add(homr);
+      int non1 = sub1.nonlinearity(r-1, upper_bound-non0);
+      if (non0 + non1 < upper_bound) {
+        upper_bound = non0 + non1;
+      }
+    }
+
+    // Find the next homogenous function of degree r.
+    // Find the minimal index i such that homr.truth_table[deg_r_x[i]] == 0.
+    int i = 0;
+    while (i < deg_r_x.size() && homr.anf[deg_r_x.at(i)] == 1) {
+      i ++;
+    }
+    // The last homogenous function of degree r.
+    if (i == deg_r_x.size()) {
+      break;
+    }
+    homr.anf[deg_r_x.at(i)] = 1;
+    for (int j = 0; j < i; j ++) {
+      homr.anf[deg_r_x.at(j)] = 0;
+    }
+    homr.set_anf_coe_done();
+  }
+
+  return upper_bound;
+}
+
+// Returns the rth-order nonlinearity.
+int BooleanFun::nonlinearity(int r) const {
+  return nonlinearity(r, (1<<n));
+}
+
---
+++ search_result.cpp
@@ -0,0 +1,216 @@
+#include "boolean_fun.h"
+#include "affine_trans.h"
+#include <set>
+#include<vector>
+#include <iostream>  // I/O 
+#include <fstream>   // file I/O
+#include <sstream>
+#include <iomanip>   // format manipulation
+#include <string>
+#include <iterator>
+using namespace std;
+
+//Our goal is to find A,b and g such that fn10(Ax+b)+g||fn6 with nl3=21. deg(g)=3
+//that is, fn10||fn6(A^-1x+b)+g(A^-1x+b) with nl3=21.
+//We have verified that the number of orbits of fn10(Ax+b) mod RM(3,6) is 65512;
+//The length of orbits of fn10(Ax) mod RM(3,6) is 888832.
+
+//read truth table and construct BooleanFun f
+void read_boolean_fun( string line, BooleanFun *f)
+{
+    stringstream stringin(line);
+    for (int i = 0; i < (1<<6); i ++) {
+      int y = 0, x = i;
+    for (int j = 0; j < 6; j ++) {
+      y = y*2 + x % 2;
+      x = x / 2;
+    }
+
+    int v;
+    stringin >> v;
+    f->set_truth_table(y, v);
+    }
+  f->set_truth_table_done();
+}
+//s1,s2 are as a 01 string of length 2^n
+string string_xor(string s1, string s2) {
+  string result = "";
+  for (int i = 0; i < (1<<6); i ++) {
+    if ((s1[i] == '0' && s2[i] == '1') ||(s1[i] == '1' && s2[i] == '0'))
+      result += "1";
+    else
+      result += "0";
+  }
+return result;
+}
+
+int main() {
+
+ifstream fin;
+ifstream fin1;
+string line;
+string line1;
+
+set<string> fn10_15;
+set<string>  fn6_6;
+
+vector<AffineTrans> all_trans;
+
+ int num=0;
+fin.open( "f10_15.txt", ios::in);  
+ if(fin.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin.eof())
+{   
+  string anf10_15;
+  BooleanFun *f1 = new BooleanFun(6);
+  getline(fin,line);
+  stringstream stringin(line);
+   //transform truth table to coefficient_anf
+  read_boolean_fun(line,f1);
+  anf10_15=f1-> get_coe_list();
+  //store coefficient ANF of F_fn10(15) in string set "fn10_15"
+  fn10_15.insert(anf10_15);  
+}
+fin.close();
+
+fin1.open( "f6_6.txt", ios::in);  
+if(fin1.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin1.eof())
+{ 
+  string anf_32;
+  BooleanFun *f1 = new BooleanFun(6);
+  getline(fin1,line1);
+  stringstream stringin(line1);
+  //transform truth table to coefficient_anf
+  read_boolean_fun(line1,f1);
+  anf_32=f1->get_coe_list();
+  //store coefficient ANF of F_fn6(6) in string set "fn6_6"
+   fn6_6.insert(anf_32);    
+} 
+fin1.close();
+
+/*int start_idx;
+int end_idx;
+cin>>start_idx;
+cin>>end_idx;*/
+
+string line_anf;
+//int count=0;
+
+set<string> G3;
+int flag=0;
+int sign=0;
+int flag1=0;
+
+//read A,b from fn10_af.txt
+//A and b are in string format, e.g.,
+// [100000 010000 001000 000100 000010 000001]100000
+fin1.open( "fn10_af.txt", ios::in);   //
+if(fin1.fail())
+  {
+    cout<<"no file!"<<endl;
+    getchar();
+    exit(0);
+  }
+while(!fin1.eof())
+{ 
+  getline(fin1,line_anf);
+ // count++;
+  bool all_in_A = false;
+  //if(count>=start_idx&&count<=end_idx)
+  //{
+      stringstream stringin(line_anf);
+      AffineTrans *T = new AffineTrans(6, line_anf);
+      set<string> magic32_trans;
+      // get A^-1
+      T->inverse();
+      for (set<string>::iterator magic32=fn6_6.begin();magic32!=fn6_6.end();magic32++) 
+    {
+        string anf2;
+        anf2= *magic32;
+        BooleanFun fm(6) ;
+        fm.set_coe_list(anf2);
+        // fm=F_fn6(6)(A^-1x)
+        fm.apply_affine_trans(*T);
+        // fm=F_fn6(6)(A^-1x) mod RM(3,6)
+        fm.trim_degree_below(3);
+        string anf=fm.get_coe_list();
+        //store coefficient_anf of fm=F_fn6(A^-1x)(6) mod RM(3,6)
+        magic32_trans.insert(anf);
+    }
+      set<string> g_s_t;
+     for(set<string>::iterator t=fn10_15.begin();t!=fn10_15.end();t++)
+     {
+        string result;
+        string s0;
+        s0=*magic32_trans.begin();
+       //let g=s0+ ti, where s0 is the first element of H3_{F_{fn6(A^-1x)}(6)}, ti\in F_fn10(15)
+        result=string_xor(*t,s0);  
+        //store coefficient_anf of g=s0+ti
+        g_s_t.insert(result);
+    } 
+
+    //traverse the set of g, of which the size is 34560
+    for (set<string>::iterator it=g_s_t.begin();it!=g_s_t.end();it++) 
+    {
+         bool all_in = true;
+         string anff=*it;
+         string result1;
+         BooleanFun valid_g(6) ;
+       
+        for (set<string>::iterator m=magic32_trans.begin();m!=magic32_trans.end();m++) 
+        {
+            string anfm;
+            //  h=g+si. where si \in H3_{F_{fn6(Ax)}(6)}
+            result1=string_xor(*it,*m);
+            //if h \notin F_fn10(15), then g is invalid.
+            if(fn10_15.find(result1)==fn10_15.end())
+            {
+              all_in = false;
+              break;
+            }       
+        }
+        //if we find a possible g. store the ANF in the string set "G3"
+        if(all_in)
+        { 
+          valid_g.set_coe_list(result1);
+          G3.insert( valid_g.get_anf());
+          flag=flag+1;
+          all_in_A = true;
+        }
+    }
+     //flag represents the number of possible g corresponding to each A,b
+      cout<< flag1++<<" "<< flag<<endl;
+      cout<< line_anf<<" "<<*magic32_trans.begin()<<endl;
+      //print valid A,b
+      if(all_in_A)
+      {
+        cout<<"A: "<<line_anf<<endl;
+        cout<<"possible g: "<<endl;
+      }
+      // print all the possbile g
+     for (set<string>::iterator n=G3.begin();n!=G3.end();n++) 
+    {
+      string anf_g1;
+      anf_g1= *n;
+      cout<<anf_g1<<endl;  
+      sign++;
+    }
+    flag=0;
+  //}
+}
+//print the number of possbile g ;
+// if there doesn't exist possbile g, then sign==0.
+cout<<"find "<<sign<<endl;
+  return 0;
+}
